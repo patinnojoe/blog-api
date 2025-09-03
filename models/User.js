2f7eec4fcf649b6504d47e6f44aef16487a9b180
Innocent Josiah Patrick <patinnojoe@Innocents-MacBook-Pro.local>
2025-09-03 10:28:34 +0100

    blog api

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {type:String, require:true},
+    email: {type: String, unique:true, trim:true, require:true},
+     password: {type:String, require:true, minLength:6,},
+
+    /**
+     * 1=>super admin
+     * 2=> admin
+     * 3=>user
+     *  */ 
+    role: {type:Number, require:true,  default:3}
+},{timestamps:true})
+
+
+const User = mongoose.model('user', userSchema)
+
+module.exports = User
